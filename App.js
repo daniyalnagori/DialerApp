00b4be18a44587161abff38f36114584231c5351
daniyalnagori <daniyalnagori@yahoo.com>
2018-09-05 08:47:26 -0700

    webrtc library implemented

--- App.js
+++ App.js
@@ -1,6 +1,16 @@
 import React, { Component } from 'react';
 import { Platform, StyleSheet, Text, View } from 'react-native';
 import Main from './src'
+var WebRTC = require('react-native-webrtc');
+var {
+  RTCPeerConnection,
+  RTCIceCandidate,
+  RTCSessionDescription,
+  RTCView,
+  MediaStream,
+  MediaStreamTrack,
+  getUserMedia,
+} = WebRTC;
 
 const instructions = Platform.select({
   ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
@@ -10,9 +20,69 @@ const instructions = Platform.select({
 });
 
 export default class App extends Component {
+  state = {
+    videoURL: null,
+    isFront: true
+  }
+
+  componentDidMount() {
+    var configuration = { "iceServers": [{ "url": "stun:stun.l.google.com:19302" }] };
+    var pc = new RTCPeerConnection(configuration);
+
+    let isFront = true;
+    MediaStreamTrack.getSources(sourceInfos => {
+      console.log(sourceInfos);
+      let videoSourceId;
+      for (const i = 0; i < sourceInfos.length; i++) {
+        const sourceInfo = sourceInfos[i];
+        if (sourceInfo.kind == "video" && sourceInfo.facing == (isFront ? "front" : "back")) {
+          videoSourceId = sourceInfo.id;
+        }
+      }
+      getUserMedia({
+        audio: true,
+        video: {
+          mandatory: {
+            minWidth: 100, // Provide your own width, height and frame rate here
+            minHeight: 100,
+            minFrameRate: 30
+          },
+          facingMode: (isFront ? "user" : "environment"),
+          optional: (videoSourceId ? [{ sourceId: videoSourceId }] : [])
+        }
+      }, (stream) => {
+        console.log('stream', stream);
+        // callback(stream);
+        // }, 
+        this.setState({
+          videoURL: stream.toURL()
+        })
+      }, logError => {
+        console.log('errorasdfa', error.message)
+      });
+    });
+
+    pc.createOffer((desc) => {
+      pc.setLocalDescription(desc, function () {
+        // Send pc.localDescription to peer
+      }, (e) => { throw e; });
+    }, (e) => { throw e; });
+
+    pc.onicecandidate = (event) => {
+      // send event.candidate to peer
+      console.log('onicaceasdf', event)
+    };
+
+  }
+
   render() {
     return (
-      <Main />
+      <View>
+
+        {/* <RTCView streamURL={this.state.videoURL} /> */}
+
+        <Main />
+      </View>
     );
   }
 }
